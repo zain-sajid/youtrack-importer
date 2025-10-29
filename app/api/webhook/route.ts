e915c96723c7712e27b029482682ec8913c34222
Zain Sajid <zsajid.bese19seecs@seecs.edu.pk>
2025-10-29 03:47:40 +0100

    chore: add logs

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -7,11 +7,15 @@ export async function POST(request: NextRequest) {
 
     switch (payload.action) {
       case "closed":
+        console.log("payload", payload.issue.labels);
         const labels = payload.issue.labels.map((label: any) => label.name);
+        console.log("labels", labels);
         const youtrackLabel = labels.find((label: any) =>
           label.startsWith("youtrack:")
         );
+        console.log("youtrackLabel", youtrackLabel);
         const youtrackIssueId = youtrackLabel.split(":")[1];
+        console.log("youtrackIssueId", youtrackIssueId);
         await updateIssueState({
           issueId: youtrackIssueId,
           stateName: "Fixed",
